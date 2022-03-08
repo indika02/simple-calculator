d8d738953891c6fe0b9fd262117a4834816801e2
indika02 <indikai002i@gmail.com>
2022-03-08 12:06:49 +0530

    change the name

---
+++ src/sample/Controller.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.Format;
+
+public class Controller {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label lblum;
+
+    @FXML
+    private Label mylabel;
+
+    @FXML
+    private TextField txtn1;
+
+    @FXML
+    private TextField txtn2;
+
+    int a,b,sum;
+
+    @FXML
+    void submit(ActionEvent event) {
+        try {
+            a = Integer.parseInt(txtn1.getText());
+            b = Integer.parseInt(txtn2.getText());
+            sum = a + b;
+            lblsum.setText(Integer.toString(sum));
+        }
+        catch (NumberFormatException e){
+           lblsum.setText("Enter only Numbers plz");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+
+    }
+
+}
